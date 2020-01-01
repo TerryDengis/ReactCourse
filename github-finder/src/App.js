078545634b955f447a4b561f8060094a70380a83
Terry Dengis <Terry.Dengis@icloud.com>
2019-12-31 23:35:01 -0700

    Initial commit

---
+++ github-finder/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+import Navbar from './component/layout/Navbar';
+import Users from './component/users/Users';
+import Search from './component/users/Search';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
