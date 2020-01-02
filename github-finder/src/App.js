3a81a8c825c9a9d5207096c324f6707c4914fcd1
Terry Dengis <Terry.Dengis@icloud.com>
2020-01-01 22:22:39 -0700

    Basic React code

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './component/layout/Navbar';
 import Users from './component/users/Users';
 import Search from './component/users/Search';
+import Alert from './component/layout/Alert';
+import About from './component/pages/About';
+import User from './component/users/User';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null
   };
 
+  // Search users
   searchUsers = async text => {
     this.setState({ loading: true });
 
@@ -23,15 +31,88 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // get a single user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
+  // get the repos for the user
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
+  // clear the users from screen
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  // show an alert
+  setAlert = (message, type) => {
+    this.setState({ alert: { message, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 2000);
+  };
+
+  // render the pages
   render() {
+    const { users, user, repos, loading } = this.state;
+
     return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github" />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar title="Github Finder" icon="fab fa-github" />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
