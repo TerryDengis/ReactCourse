51ad10dc91d5984b049aaf7cf0ce0754e4a6ee26
Terry Dengis <Terry.Dengis@icloud.com>
2020-01-02 22:44:55 -0700

    Final touches

--- github-finder/src/App.jsx
+++ github-finder/src/App.jsx
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './component/layout/Navbar';
-import Users from './component/users/Users';
-import Search from './component/users/Search';
+import User from './component/users/User';
 import Alert from './component/layout/Alert';
 import About from './component/pages/About';
-import User from './component/users/User';
+import Home from './component/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './component/pages/NotFound';
 
 import './App.css';
 
@@ -23,18 +23,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
